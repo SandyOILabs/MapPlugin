5ce29efeabe1b4e2aebada80ba973b803c0ea69b
SandyOILabs <sandeep@openinnovationslab.com>
2022-09-30 21:38:15 +0530

    21

--- lib/map_plugin.dart
+++ lib/map_plugin.dart
@@ -3,5 +3,5 @@ library map_plugin;
 // import 'models/poi_model.dart';
 
 export './test_map_plugin.dart';
-export 'http/services.dart';
-export 'models/poi_model.dart';
+export './http/services.dart';
+export './models/poi_model.dart';
